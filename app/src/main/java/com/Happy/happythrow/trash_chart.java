ec14cabf99352a7cacb16eee325aff5006b60347
Clarus23 <dnjs3023@gmail.com>
2021-11-06 06:06:39 +0900

    chart 구현

--- app/src/main/java/com/Happy/happythrow/trash_chart.java
+++ app/src/main/java/com/Happy/happythrow/trash_chart.java
@@ -3,10 +3,35 @@ package com.Happy.happythrow;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.ImageButton;
+import android.widget.TextView;
+
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.Legend.LegendForm;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.XAxis.XAxisPosition;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.components.YAxis.AxisDependency;
+import com.github.mikephil.charting.components.YAxis.YAxisLabelPosition;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.formatter.IAxisValueFormatter;
+import com.github.mikephil.charting.highlight.Highlight;
+import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
+import com.github.mikephil.charting.interfaces.datasets.IDataSet;
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
+import com.github.mikephil.charting.utils.ColorTemplate;
+import com.github.mikephil.charting.utils.MPPointF;
+
+
+import java.util.ArrayList;
 
 public class trash_chart extends AppCompatActivity {
 
@@ -17,7 +42,10 @@ public class trash_chart extends AppCompatActivity {
      */
 
     ImageButton Gotrash,GoMyBin,Gosetting;
-
+    BarChart barChart;
+    ArrayList<Integer> valueList = new ArrayList<>();
+    ArrayList<Integer> labelList = new ArrayList<>();
+    TextView minuteTextview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,7 +60,13 @@ public class trash_chart extends AppCompatActivity {
         GoMyBin = findViewById(R.id.Go_MyTrash_button);
         Gosetting = findViewById(R.id.Go_setting_button);
         Gotrash = findViewById(R.id.Go_Trash_button);
+        barChart = (BarChart)findViewById(R.id.bar_chart);
+        graphInitSetting();
 
+        BarChartGraph(labelList, valueList);
+        barChart.setTouchEnabled(false); // 확대불가
+        barChart.getAxisRight().setAxisMaxValue(100);
+        barChart.getAxisLeft().setAxisMaxValue(100);
 
         /**
          * 아래 코드는 건들지 마시오.
@@ -47,6 +81,8 @@ public class trash_chart extends AppCompatActivity {
         /**
          * 이 아래로 코드를 작성해주세요.
          */
+
+
 
         GoMyBin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -103,5 +139,75 @@ public class trash_chart extends AppCompatActivity {
                 finish();
             }
         });
+    }
+
+    public void graphInitSetting() {
+        labelList.add(1);
+        labelList.add(2);
+        labelList.add(3);
+        labelList.add(4);
+        labelList.add(5);
+        labelList.add(6);
+        labelList.add(7);
+        labelList.add(8);
+        labelList.add(9);
+        labelList.add(10);
+        labelList.add(11);
+        labelList.add(12);
+
+        valueList.add(10);
+        valueList.add(20);
+        valueList.add(30);
+        valueList.add(40);
+        valueList.add(100);
+        valueList.add(60);
+        valueList.add(90);
+        valueList.add(50);
+        valueList.add(40);
+        valueList.add(60);
+        valueList.add(20);
+        valueList.add(10);
+
+        BarChartGraph(labelList, valueList);
+        barChart.setTouchEnabled(false); // 확대불가
+        barChart.getAxisRight().setAxisMaxValue(100);
+        barChart.getAxisLeft().setAxisMaxValue(100);
+    }
+
+    private void BarChartGraph(ArrayList<Integer> labelList, ArrayList<Integer> valueList) {
+        ArrayList<BarEntry> entries = new ArrayList<>();
+        for (int i=0; i< valueList.size(); i++) {
+            entries.add(new BarEntry(i+1, (Integer) valueList.get(i)));
+        }
+
+        BarDataSet depenses = new BarDataSet(entries, "쓰레기 배출량 단위: kg");
+        depenses.setAxisDependency(YAxis.AxisDependency.LEFT);
+        barChart.setDescription(null);
+
+        ArrayList<Integer> labels = new ArrayList<Integer>();
+        for (int i=0; i < labelList.size(); i++) {
+            labels.add(labelList.get(i));
+        }
+
+        XAxis xAxis = barChart.getXAxis();
+        xAxis.setPosition(XAxisPosition.BOTTOM);
+        xAxis.setLabelCount(12);
+
+        YAxis yLAxis = barChart.getAxisLeft();
+        yLAxis.setAxisMinimum(0);
+
+        YAxis yRAxis = barChart.getAxisRight();
+        yRAxis.setDrawLabels(false);
+        yRAxis.setDrawAxisLine(false);
+        yRAxis.setDrawGridLines(false);
+
+        BarData data = new BarData(depenses);
+        depenses.setColors(ColorTemplate.LIBERTY_COLORS);
+        data.setValueTextSize(14);
+        data.setValueTypeface(Typeface.DEFAULT_BOLD);
+
+        barChart.setData(data);
+        barChart.animateXY(100, 100);
+        barChart.invalidate();
     }
 }
